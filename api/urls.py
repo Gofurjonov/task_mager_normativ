a0198033d0d0c93a5058a75e560bea403fd8ea29
Davronbek <gafurjonovdavronbek@gmail.com>
2026-04-13 19:53:35 +0500

    drf-2: Serializer va APIView CRUD qo‘shildi

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import test_api
+# from .views import test_api
+from .views import PostListCreateAPIView, PostDetailAPIView
 
 urlpatterns = [
-    path('test/', test_api),
+    # path('test/', test_api),
+    path('posts/', PostListCreateAPIView.as_view()),
+    path('posts/<int:pk>/', PostDetailAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,62 @@
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
 
-@api_view(['GET'])
-def test_api(request):
-    return Response({
-        "message": "Hello DRF"
-    })
+# @api_view(['GET'])
+# def test_api(request):
+#     return Response({
+#         "message": "Hello DRF"
+#     })
+
+class PostListCreateAPIView(APIView):
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(id=pk)
+        except Post.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        if not post:
+            return Response({"error": "Post topilmadi"}, status=404)
+
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        if not post:
+            return Response({"error": "Post topilmadi"}, status=404)
+
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        if not post:
+            return Response({"error": "Post topilmadi"}, status=404)
+
+        post.delete()
+        return Response({"message": "Post o‘chirildi"}, status=204)
